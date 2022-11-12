e3e7e8bcf5ade0e39e62b0dff5f691b6d21dcd5d
Vince Cima <contact@vincecima.com>
2022-11-12 08:01:05 -0600

    Bump version to 2.0.5

--- Casks/snail.rb
+++ Casks/snail.rb
@@ -1,6 +1,6 @@
 cask "snail" do
-  version "2.0.4"
-  sha256 "8e6e4b05c3583d68a3d625476583e600138f443f1e06b0b519bd41633c48eee2"
+  version "2.0.5"
+  sha256 "aa9cf839a88fff527610cca4acb03173d3fa482c548648c66f352b3d2ea0d05f"
 
   url "https://github.com/TheMurusTeam/Snail/releases/download/v#{version}/snail-#{version}.zip",
       verified: "github.com/TheMurusTeam/Snail"
