043fb5d8030f3365569e38ac1c78e9ac83c8b1bb
Vince Cima <contact@vincecima.com>
2024-07-28 13:23:59 -0500

    Update pcsx2.rb

--- Casks/pcsx2.rb
+++ Casks/pcsx2.rb
@@ -1,6 +1,6 @@
 cask "pcsx2" do
-  version "1.7.3479"
-  sha256 "9e3cd0fa304a013a6f18107a9be448f3ee2c44749f07725591ab503423681a04"
+  version "v2.0.2"
+  sha256 "fe751e14b80c94065ba0b0455ead59bcb50ba022078c58e8d2800e9456b0151a"
 
   url "https://github.com/PCSX2/pcsx2/releases/download/v#{version}/pcsx2-v#{version}-macos-Qt.tar.gz",
       verified: "github.com/PCSX2/pcsx2/"
@@ -9,7 +9,16 @@ cask "pcsx2" do
   desc "PCSX2 is a free and open-source PlayStation 2 (PS2) emulator."
   homepage "https://pcsx2.net/"
 
+  livecheck do
+    url "https://github.com/PCSX2/pcsx2/releases/"
+    strategy :page_match
+    regex(/pcsx2[._-]v?(\d+(?:\.\d+)+)[._-]macos[._-]Qt\.t/i)
+  end
+
   app "PCSX2-v#{version}.app"
 
-  zap trash: "~/Library/Application Support/PCSX2"
+  zap trash: [
+    "~/Library/Application Support/PCSX2",
+    "~/Library/Preferences/net.pcsx2.pcsx2.plist",
+  ]
 end
